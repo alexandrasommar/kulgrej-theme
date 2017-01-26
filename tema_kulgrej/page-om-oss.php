b843f979aa56348d5dd0c56515886bcacb9169d5
jessica <jessican.niemi@gmail.com>
2017-01-26 09:17:41 +0100

    fixat mixins så det funkar

--- tema_kulgrej/page-om-oss.php
+++ tema_kulgrej/page-om-oss.php
@@ -4,7 +4,9 @@ get_header();
 
 if (has_post_thumbnail()) { ?>
 	<section class="header-img" style="background-image:url(<?php echo the_post_thumbnail_url(); ?>);">
-		<h1 class="header-title">Om oss</h1>
+		<div>
+			<h1 class="header-title">Om oss</h1>
+		</div>
 	</section> <?php
 } ?>
 
@@ -16,10 +18,7 @@ if (has_post_thumbnail()) { ?>
 if(have_posts()) {
 	while(have_posts()) { ?>
 		<article class="about">
-			<?php
-			the_post();
-
-			?>
+			<?php the_post(); ?>
 		</article>
 			<div class="bodytext">
 				<?php the_content(); ?>
