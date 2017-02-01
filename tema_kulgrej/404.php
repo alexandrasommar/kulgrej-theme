16c011754f85f4735de4480c10f087e44530c9cd
jessica <jessican.niemi@gmail.com>
2017-02-01 10:08:12 +0100

    404

---
+++ tema_kulgrej/404.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ */
+
+get_header();
+?>
+
+<main class="not-found">
+	<section class="header-img">
+		<div class="header-bg">
+			<h1 class="header-title">Sorry!</h1>
+			<p class="not-found-text">Här fanns det inte någon kul grej</p>
+		</div>
+	</section>
+
+</main>
+
+
+
+<?php
+get_footer();
+
+
+ ?>
