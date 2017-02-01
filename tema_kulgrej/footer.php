20d32c3271371c8bbecec64893821f30e78a9120
Alexandra Sommar <alexandra@sommar.se>
2017-02-01 18:39:39 +0100

    Har gjort fullösning på kontakt-sidan, men nu syns kartan i alla fall :))))

--- tema_kulgrej/footer.php
+++ tema_kulgrej/footer.php
@@ -13,5 +13,8 @@ dynamic_sidebar( 'footer3' );
 <?php wp_footer(); ?>
 
 </footer>
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBz_iX33lVHNaRK9TG7T06En2Z6GwyiysI&callback=initMap">
+</script>
 </body>
 </html>
