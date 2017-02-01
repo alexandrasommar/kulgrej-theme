06c33501f2cac72144dac1b3fa487f5d008bc9b6
Alexandra Sommar <alexandra@sommar.se>
2017-02-01 17:56:53 +0100

    Justerar case-mallen

--- tema_kulgrej/page-case.php
+++ tema_kulgrej/page-case.php
@@ -4,21 +4,36 @@
 * Visar alla inlägg av typen kundcase.
 **/
 
-get_header();
+get_header(); ?>
 
- $cases = new WP_Query( array(
-    'post_type'       =>  'kundcase_cpt_kulgrej'
-    ) );
+<main class="main-container" style="margin-bottom: 450px;">
 
- if( $cases->have_posts() ) {
+	<?php
+	if ( have_posts() ) {
 
- 	while( $cases->have_posts() ) {
+		while ( have_posts() ) {
+			the_post();
+			the_content();
+		}
 
- 		$cases->the_post();
+	}
 
- 		the_title();
- 	}
- }
+	//kommentera ut nedan när det finns kundcase inlagt!!
+	 // $cases = new WP_Query( array(
+	 //    'post_type'       =>  'kundcase_cpt_kulgrej'
+	 //    ) );
+
+	 // if( $cases->have_posts() ) {
+
+	 // 	while( $cases->have_posts() ) {
+
+	 // 		$cases->the_post();
+
+	 // 		the_title();
+	 // 	}
+	 // } ?>
+
+ </main> <?php
 
 get_footer();
 
