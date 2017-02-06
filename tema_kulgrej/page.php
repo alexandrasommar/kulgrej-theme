98798f0c8c42e12019aaece4cca1abee5396d04d
Alexandra Sommar <alexandra@sommar.se>
2017-02-06 09:13:43 +0100

    Justerar page.php lite

--- tema_kulgrej/page.php
+++ tema_kulgrej/page.php
@@ -1,16 +1,19 @@
 <?php get_header(); ?>
-page.php
-<?php
-if(have_posts()) {
-	while(have_posts()) {
-		the_post();
 
-		if (has_post_thumbnail() ) {
-			the_post_thumbnail('medium');
+<main class="main-container" style="margin-bottom: 450px;">
+	<?php
+	if(have_posts()) {
+		while(have_posts()) {
+			the_post();
+
+			if (has_post_thumbnail() ) {
+				the_post_thumbnail('medium');
+			}
+			the_title();
+			the_content();
 		}
-		the_title();
-		the_content();
 	}
-}
-
+	?>
+</main>
+<?php
 get_footer(); ?>
