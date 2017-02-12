6dce3c2f90bd5d8c0f3c1a91d9bbecd358d9f0a6
Alexandra Sommar <alexandra@sommar.se>
2017-02-12 16:43:47 +0100

    justerar single.php lite

--- tema_kulgrej/single.php
+++ tema_kulgrej/single.php
@@ -1,16 +1,30 @@
-<?php get_header(); ?>
-single.php
-<?php
-if(have_posts()) {
-	while(have_posts()) {
-		the_post();
+<?php get_header();
 
-		if (has_post_thumbnail() ) {
-			the_post_thumbnail('medium');
-		}
-		the_title();
-		the_content();
+if (has_post_thumbnail()) { ?>
+	<section class="header-img fixed" style="background-image:url(<?php
+		if( wp_is_mobile() ) {
+			echo the_post_thumbnail_url('large');
+		} else {
+			echo the_post_thumbnail_url();
+		} ?>);">
+		<div class="header-bg">
+			<h1 class="header-title"><?php the_title(); ?></h1>
+		</div>
+	</section><?php
 	}
-}
+?>
+<main class="main-container"><?php
+	if(have_posts()) {
+		while(have_posts()) {
+			the_post(); ?>
+			<h3><?php the_title(); ?></h3><?php
+			the_content();
+		}
+	}
+	?>
 
-get_footer(); ?>
+</main><?php
+
+get_footer();
+
+?>
