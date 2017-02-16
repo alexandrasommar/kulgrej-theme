bb0d3b95d2ff32d381814c380bcfad16f6ea15fa
Alexandra Sommar <alexandra@sommar.se>
2017-02-16 19:30:47 +0100

    lägger till så kommentarer visas på page.php om de finns (theme unit test-grej)

--- tema_kulgrej/page.php
+++ tema_kulgrej/page.php
@@ -8,6 +8,10 @@
 				the_post();
 				the_content();
 
+				}
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+				    comments_template();
 				} ?>
 			</section> <?php
 		} ?>
